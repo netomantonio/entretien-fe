b1cdfd906b6d259a4b966f10e7c07b66a8b16134
[NETO] Antonio Miranda <netomantonio@gmail.com>
2023-06-03 19:15:14 -0400

    ## [ IMPROVEMENTS ] SP3H3T12 - Ajuste header menu para mobile e desktop
    1. ### componentes/HeaderLogged/*
    * Criado uma estrutura de pastas e arquivos e configurações para que cada perfil de usuário tenha a opção de um menu adequando para uma boa navegação seja via desktop ou dispositivos mobile com telas menores.

--- src/components/HeaderLogged/Admin/index.vue
+++ src/components/HeaderLogged/Admin/index.vue
@@ -1,81 +1,8 @@
 <template>
-  <div class="flex items-center justify-between w-4/5 max-w-6xl py-10">
-    <div class="w-28 lg:w-36">
-      <img alt="logo" class="w-full" src="@/assets/images/logo_white.png">
-    </div>
-    <div class="flex">
-      <ul class="flex list-none">
-        <li
-          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
-          @click="() => router.push({ name: 'Dashboard'})">
-          Dashboard
-        </li>
-        <li
-          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
-          @click="() => router.push({ name: 'User'})">
-          Usuarios
-        </li>
-        <li
-          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
-          @click="() => router.push({ name: 'Interviews'})">
-          Entrevistas
-        </li>
-        <li
-          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
-          @click="() => router.push({ name: 'Scheduling'})">
-          Agendamentos
-        </li>
-        <li
-          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
-          @click="() => router.push({ name: 'Calendar'})">
-          Calendário
-        </li>
-        <li
-          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
-          @click="() => router.push({ name: 'Profile'})">
-          Perfil
-        </li>
-        <li
-          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
-          @click="() => router.push({ name: 'Config'})">
-          Configurações
-        </li>
-        <li
-          class="px-6 py-2 font-bold bg-white rounded-full cursor-pointer focus:outline-none"
-          @click="handleLogout">
-          {{ logoutLabel }}
-        </li>
-      </ul>
-    </div>
-  </div>
+  <admin-header-logged-default/>
+  <admin-header-logged-mobile/>
+
 </template>
-<script>
-import {useRouter} from 'vue-router'
-import {computed} from 'vue'
-import {cleanCurrentUser} from '@/store/user'
-
-export default {
-  setup() {
-    const router = useRouter()
-    const logoutLabel = computed(() => {
-      const email = window.localStorage.getItem('email')
-      if (!email) {
-        return '... sair'
-      }
-      return `${email} (sair)`
-    })
-
-    function handleLogout() {
-      window.localStorage.removeItem('token')
-      cleanCurrentUser()
-      router.push({name: 'Home'})
-    }
-
-    return {
-      router,
-      logoutLabel,
-      handleLogout
-    }
-  }
-}
+<script setup>
+import AdminHeaderLoggedDefault from "@/components/HeaderLogged/Admin/Desktop/AdminHeaderLoggedDefault.vue";
 </script>
--- src/components/HeaderLogged/Candidate/index.vue
+++ src/components/HeaderLogged/Candidate/index.vue
@@ -1,71 +1,10 @@
 <template>
-  <div class="flex items-center justify-between w-4/5 max-w-6xl py-10">
-    <div class="w-28 lg:w-36">
-      <img alt="logo" class="w-full" src="@/assets/images/logo_white.png">
-    </div>
-    <div class="flex">
-      <ul class="flex list-none">
-        <li
-          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
-          @click="() => router.push({ name: 'Curriculum'})">
-          Currículo
-        </li>
-        <li
-          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
-          @click="() => router.push({ name: 'Interviews'})">
-          Entrevistas
-        </li>
-        <li
-          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
-          @click="() => router.push({ name: 'Schedules'})">
-          Agendamentos
-        </li>
-        <li
-          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
-          @click="() => router.push({ name: 'Calendar'})">
-          Calendário
-        </li>
-        <li
-          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
-          @click="() => router.push({ name: 'Profile'})">
-          Perfil
-        </li>
-        <li
-          class="px-6 py-2 font-bold bg-white rounded-full cursor-pointer focus:outline-none"
-          @click="handleLogout">
-          {{ logoutLabel }}
-        </li>
-      </ul>
-    </div>
-  </div>
+  <candidate-header-logged-default/>
+  <candidate-header-logged-mobile/>
+
 </template>
-<script>
-import {useRouter} from 'vue-router'
-import {computed} from 'vue'
-import {cleanCurrentUser} from '@/store/user'
 
-export default {
-  setup() {
-    const router = useRouter()
-    const logoutLabel = computed(() => {
-      const email = window.localStorage.getItem('email')
-      if (!email) {
-        return '... sair'
-      }
-      return `${email} (sair)`
-    })
-
-    function handleLogout() {
-      window.localStorage.removeItem('token')
-      cleanCurrentUser()
-      router.push({name: 'Home'})
-    }
-
-    return {
-      router,
-      logoutLabel,
-      handleLogout
-    }
-  }
-}
+<script setup>
+import CandidateHeaderLoggedDefault from "@/components/HeaderLogged/Candidate/Desktop/CandidateHeaderLoggedDefault.vue";
+import CandidateHeaderLoggedMobile from "@/components/HeaderLogged/Candidate/Mobile/CandidateHeaderLoggedMobile.vue";
 </script>
