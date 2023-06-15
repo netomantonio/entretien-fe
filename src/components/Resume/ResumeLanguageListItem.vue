95508e5e342224627b8345d8e968ef2e5286ce83
Leonardo Wrobel <leonardo.wrobel@tuntscorp.com>
2023-06-15 11:15:48 -0300

    fix(SP3T4): correção no nome do atríbuto

--- src/components/Resume/ResumeLanguageListItem.vue
+++ src/components/Resume/ResumeLanguageListItem.vue
@@ -13,7 +13,7 @@
       <div class="sm:col-span-1">
         <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Nível</label>
         <div class="mt-2">
-          <select id="country" name="language-level" autocomplete="language-level" v-model="value.level"
+          <select id="country" name="language-level" autocomplete="language-level" v-model="value.languageProficiencyLevel"
                   class="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
             <option v-for="level in LanguageProficiencyLevels" v-bind:key="level">{{ level }}</option>
           </select>
