58c9a119b435b054fed21edd818931d91c836003
Leonardo Wrobel <leonardo.wrobel@tuntscorp.com>
2023-06-15 11:12:44 -0300

    feat(SP3T4): imports de novos ícones

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import App from './App.vue'
 /* fontawesome */
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
-import { faUser, faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faPenToSquare, faTrash, faCalendarDays, faGraduationCap, faBriefcase, faPlus, faEarthAmerica } from "@fortawesome/free-solid-svg-icons";
 // @ts-ignore
 import router from './router'
 // @ts-ignore
@@ -16,7 +16,7 @@ import '@/assets/css/tailwind.css'
 import '@/assets/css/fonts.css'
 import 'vue-toastification/dist/index.css'
 
-library.add(faUser, faPenToSquare, faTrash);
+library.add(faUser, faPenToSquare, faTrash, faCalendarDays, faGraduationCap, faBriefcase, faPlus, faEarthAmerica);
 
 const app = createApp(App)
 app.use(router)
