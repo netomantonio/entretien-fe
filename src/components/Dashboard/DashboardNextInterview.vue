cbfeffb62dee32b8d9a6dfd245ad5c4e3ae0daac
Leonardo Wrobel <leonardo.wrobel@tuntscorp.com>
2023-06-19 00:02:03 -0300

    wip(SP3T5): ajustes

--- src/components/Dashboard/DashboardNextInterview.vue
+++ src/components/Dashboard/DashboardNextInterview.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class=''>
-    <div class="flex">
+  <div class='flex-column'>
+    <div class="flex grow-0">
       <font-awesome-icon icon="circle-exclamation" class="text-gray-600 text-xl mr-2 mt-1"/>
       <h5 v-if="state.nextInterview" class="text-gray-600 text-lg">Próxima entrevista</h5>
       <h5 v-else class="text-gray-600 text-lg">Fique atento!</h5>
@@ -9,8 +9,8 @@
       <div>
         <span class="text-9xl font-bold">{{state.nextInterview.startingAt.split("T")[0].slice(5).replaceAll("-", "/")}}</span>
       </div>
-      <div class="flex-col-reverse ml-5">
-        <div class="self-end">
+      <div class="flex-col align-self-center mt-3 ml-5">
+        <div class="self-end ">
           <span class="text-sm">Horário:</span>
         </div>
         <div class="self-end">
@@ -18,8 +18,8 @@
         </div>
       </div>
     </div>
-    <div v-if="state.resumeLastInterview" class="mt-3 text-gray-600">
-      <span>Última atualização do cúrriculo em </span><span class="font-bold">{{ new Date(state.resumeLastInterview).toLocaleDateString() }}</span>
+    <div v-if="state.resumeLastInterview" class="mt-3 text-gray-600 flex">
+      <span>Última atualização do cúrriculo em </span><span class="font-bold ml-1">{{new Date(state.resumeLastInterview).toLocaleDateString() }}</span>
     </div>
   </div>
 </template>
