0bc61912e0b3b9d07bad13cf5e6fb99fd1dd55f6
Leonardo Wrobel <leonardo.wrobel@tuntscorp.com>
2023-06-19 21:50:07 -0300

    feat(SP3T5): dash para o candidato

--- src/components/Dashboard/index.vue
+++ src/components/Dashboard/index.vue
@@ -4,18 +4,29 @@
     <DashboardHeader/>
     <div class="px-6 pt-6 grid gap-6 md:grid-cols-2 lg:grid-cols-2">
       <div class="md:col-span-2 lg:col-span-1">
-        <DashboardCalendar />
+        <DashboardNextInterview/>
       </div>
-      <div class="md:col-span-2 lg:col-span-1">
-        <DashboardNextInterview />
+      <div class="hidden md:col-span-2 lg:col-span-1">
+        <DashboardCalendar/>
+      </div>
+    </div>
+    <div class="md:mt-5 px-6 pt-6 grid lg:gap-6 md:grid-cols-3 lg:grid-cols-3">
+      <div class="md:col-span-2 lg:col-span-2 mr-5">
+        <InterviewHistory/>
+      </div>
+      <div class="md:col-span-1 lg:col-span-1">
+        <DashboardCandidateNumbersCard/>
       </div>
     </div>
   </div>
 </template>
 <script setup>
 import DashboardHeader from "@/components/Dashboard/DashboardHeader.vue";
-import DashboardCalendar from "@/components/Dashboard/DashboardCalendar.vue";
+import DashboardCalendar from "@/components/Dashboard/DashboardCalendar/DashboardCalendar.vue";
 import DashboardNextInterview from "@/components/Dashboard/DashboardNextInterview.vue";
+import InterviewHistory from "@/components/Dashboard/InterviewHistory/index.vue";
+import DashboardCandidateNumbersCard from "@/components/Dashboard/DashboardStats/DashboardCandidateNumbersCard.vue";
+
 </script>
 <style scoped>
 </style>
