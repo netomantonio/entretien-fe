38e9f3763860e40f448a0d3802a6402f66d1447c
Leonardo Wrobel <leonardo.wrobel@tuntscorp.com>
2023-06-19 21:49:46 -0300

    feat(SP3T5): services

--- src/services/interview.js
+++ src/services/interview.js
@@ -140,5 +140,19 @@ export default httpClient => ({
       data: response.data,
       errors: errors
     }
+  },
+  getCandidateInterviewStats: async () => {
+    const response = await httpClient.get(`/api/interview/candidate/stats`)
+    let errors = null
+    if (!response.data) {
+      errors = {
+        status: response.request.status,
+        statusText: response.request.statusText
+      }
+    }
+    return {
+      data: response.data,
+      errors: errors
+    }
   }
 })
