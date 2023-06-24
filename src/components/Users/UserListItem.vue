5a833276608738456647a3c6d2c085f73e96a7c3
Leonardo Wrobel <leonardo.wrobel@tuntscorp.com>
2023-06-24 17:18:54 -0300

    feat(SP3HFT6): novos componentes e ícone;

---
+++ src/components/Users/UserListItem.vue
@@ -0,0 +1,81 @@
+<template>
+  <td class="td-container">
+    {{ props.user.firstName + " " + props.user.lastName }}
+  </td>
+  <td :class="`td-container text-${getUserTypeColor()}`">
+    <font-awesome-icon icon="circle-user"/>
+    {{getUserTypeName()}}
+  </td>
+  <td class="td-container">
+    {{new Date(props.user.createdAt).toLocaleDateString()}}
+  </td>
+  <td class="td-container">
+    {{new Date(props.user.updatedAt).toLocaleDateString()}}
+  </td>
+  <td class="td-container">
+    {{props.user.email}}
+  </td>
+  <td class="td-container">
+    {{getPhone()}}
+  </td>
+  <td class="td-container">
+    {{getCpf()}}
+  </td>
+</template>
+
+<script setup>
+const props = defineProps({
+  user: {}
+})
+
+function getUserTypeColor() {
+  switch (props.user.roles[0].name) {
+    case "ROLE_CANDIDATE":
+      return "lightseagreen-500"
+    case "ROLE_RECRUITER":
+      return "cornflowerblue-500"
+    case "ROLE_MANAGER":
+      return "darkgoldenrod-500"
+    case "ROLE_ADMIN":
+      return "deeppink-600"
+    default:
+      return "gray-500"
+  }
+}
+
+function getUserTypeName(){
+  const roleName = props.user.roles[0].name.split("_")[1].toLowerCase()
+  return roleName.charAt(0).toUpperCase() + roleName.slice(1)
+}
+
+function getPhone(){
+  const phone = props.user.phone.replace(/\D/g,'')
+  if(phone.match(/^ *$/) !== null)
+    return "Indisponível"
+  const toShow = phone.match(/(\d{2})(\d{4})(\d{4})/);
+  return '(' + toShow[1] + ') ' + toShow[2] + '-' + toShow[3]
+}
+
+function getCpf(){
+  const cpf = props.user.cpf.replace(/\D/g,'')
+  if(cpf.match(/^ *$/) !== null)
+    return " - "
+
+  const toShow = cpf.match(/(\d{3})(\d{3})(\d{3})(\d{2})/);
+  return toShow[1] + '. ' + toShow[2] + '.' + toShow[3] + '-' + toShow[3]
+}
+
+</script>
+
+<style scoped>
+.td-container {
+  padding: 1rem 1rem;
+  --bs-bg-opacity: 1;
+  background-color: rgba(var(--bs-white-rgb), var(--bs-bg-opacity));
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  --tw-border-opacity: 1;
+  border-color: rgba(229, 231, 235, var(--tw-border-opacity));
+  border-bottom-width: 1px;
+}
+</style>
---
+++ src/components/Users/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="flex w-full p-5">
+    <div class="p-8 rounded-md w-full">
+      <h2 class="text-gray-600 font-semi-bold">Usuários</h2>
+      <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+        <div class="inline-block min-w-full shadow px-2 py-1 rounded-lg overflow-hidden">
+          <table v-if="state.users" class="min-w-full leading-normal">
+            <thead>
+            <tr>
+              <ListHeader v-for="item in headerList" :key="item" :customStyle="item.style" :title="item.name"/>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="user in state.users" v-bind:key="user.id">
+              <UserListItem :user="user"/>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {reactive} from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import UserListItem from "@/components/Users/UserListItem.vue";
+import services from '@/services'
+import {getGlobalLoading} from "@/store/global";
+import ListHeader from "@/components/Users/ListHeader.vue";
+
+const headerList = [
+  {name: "Nome", style: null},
+  {name: "", style: null},
+  {name: "Criado em", style: "text-sm text-gray-500 font-normal"},
+  {name: "Atualizado em", style: "text-sm text-gray-500 font-normal"},
+  {name: "Email", style: null},
+  {name: "Telefone", style: null},
+  {name: "CPF", style: null},
+]
+
+const state = reactive({
+  users: {}
+})
+
+async function loadUsers() {
+  const {data} = await services.users.getUsers()
+  state.users = data
+}
+
+loadUsers()
+</script>
